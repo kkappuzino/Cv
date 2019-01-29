02acb8e237d4059491b6f0c2388b4c221af4e8bd
Venla Paanila <venla.p123@gmail.com>
2019-01-29 14:15:13 +0200

    something

---
+++ Structure/home.php
@@ -0,0 +1,81 @@
+<section id="home">
+    <div class="bottom-left ml-0 ml-sm-5">
+        <span class="glitch" data-text="VENLA PAANILA">
+            Venla Paanila   
+        </span>
+        <span class="glitch2" data-text="WEB DEVELOPER">
+            Web Developer  /
+        </span>
+        <span class="glitch3" data-text="ARTIST">
+              Artist
+        </span>
+    </div>
+</section>
+
+<section id=skills>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="glitch3" data-text="skills">Skills</h2>
+            </div>
+            <div class="col-12 col-sm">
+                <h3 class="text-center">Html</h3>
+            </div>
+            <div class="col col-12 align-self-center pb-5">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+                    85%
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm">
+                <h3 class="text-center">Css</h3>
+            </div>
+            <div class="col col-12 align-self-center pb-5">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+                    85%
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm">
+                <h3 class="text-center">PHP</h3>
+            </div>
+            <div class="col col-12 align-self-center pb-5">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                    80%
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm">
+                <h3 class="text-center">Javascript</h3>
+            </div>
+            <div class="col col-12 align-self-center pb-5">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                    50%
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <h2 class="glitch3 my-5" data-text="I also know these things" >I also know these things :</h2>
+            </div><br>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  SCSS</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  Wordpress</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  Java</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  Gitbash</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  less</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  Sql</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  Office 365</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-check-square"></i>  Bootstrap</h3></div>
+            <div class="col-12">
+                <h2 class="glitch2 my-5" data-text="Things I'd like to learn" >Things I'd like to learn :</h2>
+            </div><br>
+            <div class="col col-12 col-sm-3 "><h3><i class="far fa-square " ></i>  Languages</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-square"></i>  Ux/ui</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-square "></i>  Piano</h3></div>
+            <div class="col col-12 col-sm-3"><h3><i class="far fa-square"></i>  Product desing</h3></div>
+        </div>
+    </div>
+</section>
